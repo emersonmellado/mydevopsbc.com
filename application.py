bab3cbe785d989bd02b3737a17425c86db6f0ecf
Emerson Mellado <emersonmellado@gmail.com>
2020-07-31 19:44:50 -0700

    Make the bg red

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ header_text = '''
     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
 instructions = '''
     <p>
-    <h1 style="padding: 60px; background-color: blue; color: white">Hello people!</h1>
+    <h1 style="padding: 60px; background-color: red; color: white">Hello people!</h1>
     <br/>
     <h1>Imagine something really cool here :)</h1>
     <hr/>
