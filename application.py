489ff9209a0801ea9583e7347118ff0ec35521af
Emerson Mellado <3016337+emersonmellado@users.noreply.github.com>
2020-07-31 17:35:42 -0700

    Update application.py

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ header_text = '''
     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
 instructions = '''
     <p>
-    <h1 style="padding: 60px; background-color: gray; color: white">Hello people!</h1>
+    <h1 style="padding: 60px; background-color: pink; color: white">Hello people!</h1>
     <br/>
     <h1>Imagine something really cool here :)</h1>
     <hr/>
